f81f3906dbf7ca3a120eb2ed1d43b150e151aa7d
PhuScrum <s3686970@rmit.edu.vn>
2021-01-15 12:11:56 +0700

    *add*

--- Guess.java
+++ Guess.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * You need to implement an algorithm to make guesses
  * for a 4-digits number in the method make_guess below
@@ -5,15 +9,34 @@
  * PLEASE DO NOT CHANGE THE NAME OF THE CLASS AND THE METHOD
  */
 public class Guess {
-	
+	static int myguess =1234;
+	static int totalGuess = 0;
+	static int bothGuessStrike = 0;
+	Map<String, String> h = new HashMap<String, String>() {{
+		put("a","b");
+	}};
+	public static int firstGuess(){
+		int firstguess=1234;
+		return firstguess;
+	}
+
 	public static int make_guess(int hits, int strikes) {
-		// just a dummy guess
-		int myguess = 1000;
-		
+		bothGuessStrike = hits+strikes;
+		myguess= firstGuess();
+		/**
+		 * first guess
+		 *
+		 */
+		while(totalGuess==1){
+			if(bothGuessStrike==0){
+				myguess= 5678;
+			}
+		}
+
 		/*
 		 * IMPLEMENT YOUR GUESS STRATEGY HERE
 		 */
-		 
+		totalGuess++;
 		return myguess;
 	}
 }
